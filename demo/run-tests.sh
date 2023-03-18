30f51e2c09042ee323e023c87af2bd25d38e6fad
Till Kamppeter <till.kamppeter@gmail.com>
2023-03-18 23:41:04 +0100

    Added test script for "make test"/"make check"
    The script demo/run-tests.sh runs the demo/print_frontend text mode
    example frontend and stops it by supplying "stop" to its standard
    input.  If the frontend does not stop, it gets killed after a
    timeout. The log of the frontend is checked whether it got actually
    stopped by the "stop" command or not.
    
    We cannot do actual backend discovery and print tests as we cannot
    make the build of cpdb-libs dependent on any backend, due to the fact
    that all backends depend on cpdb-libs. This circular dependency would
    break bootstrapping (first complete build) a distro ...

---
+++ demo/run-tests.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e -u
+
+LOG=run-tests.log
+CPDBDEMO=./print_frontend
+
+export LD_LIBRARY_PATH=`pwd`/cpdb/.libs
+
+cleanup() {
+    # Show log
+    cat $LOG
+    # Remove the log file
+    rm -f $LOG
+}
+
+trap cleanup 0 EXIT INT QUIT ABRT PIPE TERM
+
+# Build demo/test frontend
+cd demo
+make $CPDBDEMO
+
+# Create the log file
+rm -f $LOG
+touch $LOG
+
+# Run the demo with a session D-Bus and feed in commands.
+( \
+  sleep 1; \
+  echo stop \
+) | dbus-run-session -- $CPDBDEMO > $LOG 2>&1 &
+
+# Give the frontned a maximum of 5 seconds to run and then kill it, to avoid
+# the script getting stuck if stopping it fails.
+i=0
+FRONTEND_PID=$!
+while kill -0 $FRONTEND_PID >/dev/null 2>&1; do
+    i=$((i+1))
+    if test $i -ge 5; then
+	kill -KILL $FRONTEND_PID >/dev/null 2>&1 || true
+	echo "FAIL: Frontend keeps running!"
+	exit 1
+    fi
+    sleep 1
+done
+
+
+if grep -q "Stopping front end" $LOG; then
+    echo "Frontend correctly shut down"
+else
+    echo "FAIL: Frontend not correctly stopped!"
+    exit 1
+fi
+
+echo "SUCCESS!"
+
+exit 0
